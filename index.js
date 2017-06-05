8f2bc384c4b3b33e1f996700d62ac3efafd18e9e
Kyle Brown <brow1144@purdue.edu>
2017-06-05 17:28:27 -0400

    Add a color input and change background color to that

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ function handleSubmit(e) {
     e.preventDefault()
     const f = e.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
-
+    heading.textContent = f.personName.value + 's favorite color is ' + f.colorName.value
+    /*const color = document.getElementById('colorLabel')
+    color.className = 'cool-stuff'*/
+    document.body.style.backgroundColor = f.colorName.value;
 }
 
 personForm.addEventListener('submit', handleSubmit)
