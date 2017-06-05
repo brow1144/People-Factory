d51c1cf931d90b29c83e26624d0a6396cd975655
Kyle Brown <brow1144@purdue.edu>
2017-06-05 19:22:36 -0400

    Add more customized color to the page

--- index.js
+++ index.js
@@ -6,9 +6,26 @@ function handleSubmit(e) {
     const f = e.target
     const heading = document.querySelector('h1')
     heading.textContent = f.personName.value + 's favorite color is ' + f.colorName.value
-    /*const color = document.getElementById('colorLabel')
-    color.className = 'cool-stuff'*/
-    document.body.style.backgroundColor = f.colorName.value;
+
+    document.body.style.backgroundColor = f.colorName.value
+    
+    //Input was being difficult so I added a for loop to change all of them
+    const inputBackground = document.getElementsByTagName('input')
+    for(var i = 0; i < inputBackground.length; i++) {
+        if(inputBackground[i].type == "text") {
+            inputBackground[i].style.backgroundColor = f.colorName.value;
+            inputBackground[i].style.borderBottom = '2px solid #585858'
+        }
+    }
+
+    const button = document.getElementsByTagName('button')
+    for(var i = 0; i < button.length; i++) {
+        if(button[i].type == "submit") {
+            button[i].style.backgroundColor = "#585858";
+        }
+    }
+
+
 }
 
 personForm.addEventListener('submit', handleSubmit)
